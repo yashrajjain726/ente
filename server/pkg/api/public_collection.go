93e790b2d911bae92ddbcda4e3223b78277b8c55
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2026-02-26 09:46:16 +0530

    Add endpoint for getting uploadurl with md5 and size

--- server/pkg/api/public_collection.go
+++ server/pkg/api/public_collection.go
@@ -137,6 +137,27 @@ func (h *PublicCollectionHandler) GetUploadUrls(c *gin.Context) {
 	})
 }
 
+// GetUploadURLV2 returns a single upload URL that enforces checksum + content-length headers
+func (h *PublicCollectionHandler) GetUploadURLV2(c *gin.Context) {
+	enteApp := auth.GetApp(c)
+	var req ente.UploadURLRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handler.Error(c, stacktrace.Propagate(err, ""))
+		return
+	}
+	collection, err := h.Controller.GetPublicCollection(c, true)
+	if err != nil {
+		handler.Error(c, stacktrace.Propagate(err, ""))
+		return
+	}
+	url, err := h.FileCtrl.GetUploadURLWithMetadata(c, collection.Owner.ID, req, enteApp)
+	if err != nil {
+		handler.Error(c, stacktrace.Propagate(err, ""))
+		return
+	}
+	c.JSON(http.StatusOK, url)
+}
+
 // GetMultipartUploadURLs returns upload Urls where files can be uploaded
 func (h *PublicCollectionHandler) GetMultipartUploadURLs(c *gin.Context) {
 	enteApp := auth.GetApp(c)
