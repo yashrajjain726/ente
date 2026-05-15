5d9321342d506ffe8748b08f31cbe867e1283fca
Anand Baburajan <anandbaburajan@gmail.com>
2026-05-15 13:21:49 +0530

    Fold social wall migrations into initial schema

--- server/migrations/122_wall_multiple_links.up.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE wall_links
-    DROP CONSTRAINT IF EXISTS wall_links_pkey;
-
-ALTER TABLE wall_links
-    ADD PRIMARY KEY (wall_id, auth_key_hash);
-
-CREATE INDEX IF NOT EXISTS idx_wall_links_wall_updated
-    ON wall_links (wall_id, updated_at DESC);
