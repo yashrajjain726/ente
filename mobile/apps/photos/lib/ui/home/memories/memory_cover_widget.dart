a78986d88669f12b8253bed828a82d615c50a057
Priyanshu Dangare <priyanshu@ente.com>
2026-07-06 13:20:49 +0530

    [mobile][photos] use bold font for non-time memories and remove shadow from cards
    Signed-off-by: Priyanshu Dangare <priyanshu@ente.com>

--- mobile/apps/photos/lib/ui/home/memories/memory_cover_widget.dart
+++ mobile/apps/photos/lib/ui/home/memories/memory_cover_widget.dart
@@ -2,34 +2,28 @@ import "package:ente_components/ente_components.dart";
 import "package:ente_pure_utils/ente_pure_utils.dart";
 import "package:flutter/material.dart";
 import "package:photos/core/constants.dart";
-import "package:photos/models/memories/memory.dart";
+import "package:photos/models/memories/smart_memory.dart";
 import "package:photos/theme/colors.dart";
-import "package:photos/theme/effects.dart";
 import "package:photos/theme/ente_theme.dart";
 import "package:photos/ui/home/memories/all_memories_page.dart";
 import "package:photos/ui/home/memories/memory_cover_util.dart";
 import "package:photos/ui/viewer/file/thumbnail_widget.dart";
 
-// TODO: Use a single instance variable for `allMemories` and `allTitles`
 class MemoryCoverWidget extends StatefulWidget {
-  final List<Memory> memories;
-  final List<List<Memory>> allMemories;
+  final SmartMemory smartMemory;
+  final List<SmartMemory> allMemories;
   final double height;
   final double width;
   static const outerStrokeWidth = 1.0;
   static const aspectRatio = 236.924 / 163.214;
   static const gap = 5.0;
-  final String title;
-  final List<String> allTitle;
   final int currentMemoryIndex;
 
   const MemoryCoverWidget({
-    required this.memories,
+    required this.smartMemory,
     required this.allMemories,
     required this.height,
     required this.width,
-    required this.title,
-    required this.allTitle,
     required this.currentMemoryIndex,
     super.key,
   });
@@ -43,16 +37,21 @@ class _MemoryCoverWidgetState extends State<MemoryCoverWidget> {
   Widget build(BuildContext context) {
     //memories will be empty if all memories are deleted and setState is called
     //after FullScreenMemory screen is popped
-    if (widget.memories.isEmpty) {
+    final memories = widget.smartMemory.memories;
+    if (memories.isEmpty) {
       return const SizedBox.shrink();
     }
 
-    final index = getNextMemoryIndex(widget.memories);
-    final title = widget.title;
+    final index = getNextMemoryIndex(memories);
+    final title = widget.smartMemory.title;
 
-    final memory = widget.memories[index];
+    final memory = memories[index];
     final isSeen = memory.isSeen();
-    final brightness = Theme.of(context).brightness;
+    final titleFontWeight =
+        widget.smartMemory.type == MemoryType.time ||
+            widget.smartMemory.type == MemoryType.filler
+        ? FontWeight.w300
+        : FontWeight.w700;
 
     return Padding(
       padding: const EdgeInsets.symmetric(
@@ -66,7 +65,6 @@ class _MemoryCoverWidgetState extends State<MemoryCoverWidget> {
             AllMemoriesPage(
               initialPageIndex: widget.currentMemoryIndex,
               allMemories: widget.allMemories,
-              allTitles: widget.allTitle,
             ),
           );
           if (!mounted) return;
@@ -76,15 +74,6 @@ class _MemoryCoverWidgetState extends State<MemoryCoverWidget> {
           height: widget.height,
           width: widget.width,
           decoration: BoxDecoration(
-            boxShadow: brightness == Brightness.dark
-                ? [
-                    const BoxShadow(
-                      color: strokeFainterDark,
-                      spreadRadius: MemoryCoverWidget.outerStrokeWidth,
-                      blurRadius: 0,
-                    ),
-                  ]
-                : [...shadowFloatFaintestLight],
             borderRadius: BorderRadius.circular(22),
           ),
           child: ClipRRect(
@@ -133,11 +122,11 @@ class _MemoryCoverWidgetState extends State<MemoryCoverWidget> {
                           child: Text(
                             title,
                             style: getEnteTextTheme(context).body.copyWith(
-                              fontSize: widget.height * 0.095,
+                              fontSize: widget.height * 0.085,
                               fontFamily: TextStyles.outfitFontFamily,
                               package: TextStyles.fontPackage,
                               color: isSeen ? textFaintDark : Colors.white,
-                              fontWeight: .w400,
+                              fontWeight: titleFontWeight,
                             ),
                             textAlign: TextAlign.left,
                           ),
