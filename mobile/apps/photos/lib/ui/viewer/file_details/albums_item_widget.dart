8a4004b6712db53177ce78e3ffa12d330ddc76b8
aman-pilot <aman.test.69@gmail.com>
2026-07-08 13:07:18 +0530

    Refactor AlbumsItemWidget to enhance UI and improve folder display logic

--- mobile/apps/photos/lib/ui/viewer/file_details/albums_item_widget.dart
+++ mobile/apps/photos/lib/ui/viewer/file_details/albums_item_widget.dart
@@ -1,18 +1,17 @@
+import "package:ente_components/ente_components.dart";
 import "package:ente_pure_utils/ente_pure_utils.dart";
 import "package:flutter/material.dart";
+import "package:hugeicons/hugeicons.dart";
 import "package:logging/logging.dart";
 import "package:photos/core/event_bus.dart";
 import "package:photos/db/files_db.dart";
 import "package:photos/events/pause_video_event.dart";
 import "package:photos/generated/l10n.dart";
-import 'package:photos/models/collection/collection.dart';
 import 'package:photos/models/collection/collection_items.dart';
 import 'package:photos/models/file/file.dart';
 import "package:photos/models/selected_files.dart";
 import "package:photos/services/collections_service.dart";
 import "package:photos/ui/collections/collection_action_sheet.dart";
-import "package:photos/ui/components/buttons/chip_button_widget.dart";
-import "package:photos/ui/components/info_item_widget.dart";
 import "package:photos/ui/viewer/gallery/collection_page.dart";
 
 class AlbumsItemWidget extends StatelessWidget {
@@ -24,7 +23,6 @@ class AlbumsItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final fileIsBackedup = file.uploadedFileID == null ? false : true;
     late Future<Set<int>> allCollectionIDsOfFile;
-    //Typing this as Future<Set<T>> as it would be easier to implement showing multiple device folders for a file in the future
     final Future<Set<String>> allDeviceFoldersOfFile = Future.sync(
       () => {file.deviceFolder ?? ''},
     );
@@ -33,32 +31,46 @@ class AlbumsItemWidget extends StatelessWidget {
         file.uploadedFileID!,
       );
     }
-    return InfoItemWidget(
+    return Column(
       key: const ValueKey("Albums"),
-      leadingIcon: Icons.folder_outlined,
-      title: AppLocalizations.of(context).albums,
-      subtitleSection: fileIsBackedup
-          ? _collectionsListOfFile(
-              context,
-              allCollectionIDsOfFile,
-              currentUserID,
-            )
-          : _deviceFoldersListOfFile(allDeviceFoldersOfFile),
-      hasChipButtons: true,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(AppLocalizations.of(context).albums, style: TextStyles.h2),
+        const SizedBox(height: Spacing.lg),
+        FutureBuilder<List<Widget>>(
+          future: fileIsBackedup
+              ? _collectionsListOfFile(
+                  context,
+                  allCollectionIDsOfFile,
+                  currentUserID,
+                )
+              : _deviceFoldersListOfFile(context, allDeviceFoldersOfFile),
+          builder: (context, snapshot) {
+            final chips = snapshot.data ?? const <Widget>[];
+            return Wrap(
+              spacing: Spacing.sm,
+              runSpacing: Spacing.sm,
+              children: chips,
+            );
+          },
+        ),
+      ],
     );
   }
 
-  Future<List<ChipButtonWidget>> _deviceFoldersListOfFile(
+  Future<List<Widget>> _deviceFoldersListOfFile(
+    BuildContext context,
     Future<Set<String>> allDeviceFoldersOfFile,
   ) async {
     try {
-      final chipButtons = <ChipButtonWidget>[];
+      final chips = <Widget>[];
       final List<String> deviceFolders = (await allDeviceFoldersOfFile)
           .toList();
       for (var deviceFolder in deviceFolders) {
-        chipButtons.add(ChipButtonWidget(deviceFolder));
+        chips.add(FilterChipComponent(label: deviceFolder, onChanged: (_) {}));
       }
-      return chipButtons;
+      return chips;
     } catch (e, s) {
       Logger(
         "AlbumsItemWidget",
@@ -67,26 +79,26 @@ class AlbumsItemWidget extends StatelessWidget {
     }
   }
 
-  Future<List<ChipButtonWidget>> _collectionsListOfFile(
+  Future<List<Widget>> _collectionsListOfFile(
     BuildContext context,
     Future<Set<int>> allCollectionIDsOfFile,
     int currentUserID,
   ) async {
     try {
-      final chipButtons = <ChipButtonWidget>[];
+      final colors = context.componentColors;
+      final chips = <Widget>[];
       final Set<int> collectionIDs = await allCollectionIDsOfFile;
-      final collections = <Collection>[];
       for (var collectionID in collectionIDs) {
-        final c = CollectionsService.instance.getCollectionByID(collectionID);
-        collections.add(c!);
-        chipButtons.add(
-          ChipButtonWidget(
-            c.isHidden() ? AppLocalizations.of(context).hidden : c.displayName,
-            onTap: () {
+        final c = CollectionsService.instance.getCollectionByID(collectionID)!;
+        chips.add(
+          FilterChipComponent(
+            label: c.isHidden()
+                ? AppLocalizations.of(context).hidden
+                : c.displayName,
+            onChanged: (_) {
               if (c.isHidden()) {
                 return;
               }
-              // stop playing video when navigating to collection page
               Bus.instance.fire(PauseVideoEvent());
               routeToPage(
                 context,
@@ -99,13 +111,15 @@ class AlbumsItemWidget extends StatelessWidget {
           ),
         );
       }
-
-      // Add the '+' button if feature flag is enabled
-      chipButtons.add(
-        ChipButtonWidget(
-          null,
-          leadingIcon: Icons.add,
-          iconSize: 15,
+      chips.add(
+        IconButtonComponent(
+          icon: HugeIcon(
+            icon: HugeIcons.strokeRoundedPlusSign,
+            size: IconSizes.small,
+            color: colors.textBase,
+          ),
+          variant: IconButtonComponentVariant.circular,
+          shouldSurfaceExecutionStates: false,
           onTap: () {
             final selectedFiles = SelectedFiles();
             selectedFiles.files.add(file);
@@ -117,8 +131,7 @@ class AlbumsItemWidget extends StatelessWidget {
           },
         ),
       );
-
-      return chipButtons;
+      return chips;
     } catch (e, s) {
       Logger(
         "AlbumsItemWidget",
