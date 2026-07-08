4d505a478ddc9318c8bd044e26f4fffd4e5102c8
aman-pilot <aman.test.69@gmail.com>
2026-07-08 15:44:56 +0530

    update edit sheet

--- mobile/apps/photos/lib/ui/viewer/date/date_time_picker.dart
+++ mobile/apps/photos/lib/ui/viewer/date/date_time_picker.dart
@@ -1,3 +1,4 @@
+import "package:ente_components/ente_components.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:photos/generated/l10n.dart";
@@ -9,37 +10,22 @@ Future<DateTime?> showDatePickerSheet(
   DateTime? maxDate,
   DateTime? minDate,
   bool startWithTime = false,
-}) async {
-  final colorScheme = getEnteColorScheme(context);
-  final sheet = Container(
-    decoration: BoxDecoration(
-      color: colorScheme.backgroundElevated,
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(12),
-        topRight: Radius.circular(12),
-      ),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: DateTimePickerWidget(
-        (DateTime dateTime) {
-          Navigator.of(context).pop(dateTime);
-        },
-        () {
-          Navigator.of(context).pop(null);
-        },
+}) {
+  return showBottomSheetComponent<DateTime?>(
+    context: context,
+    builder: (context) => BottomSheetComponent(
+      showCloseButton: true,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      content: DateTimePickerWidget(
+        (dateTime) => Navigator.of(context).pop(dateTime),
+        () => Navigator.of(context).pop(null),
         initialDate,
         minDateTime: minDate,
         maxDateTime: maxDate,
+        startWithTime: startWithTime,
       ),
     ),
   );
-  final newDate = await showModalBottomSheet<DateTime?>(
-    context: context,
-    isScrollControlled: true,
-    builder: (context) => sheet,
-  );
-  return newDate;
 }
 
 class DateTimePickerWidget extends StatefulWidget {
@@ -49,6 +35,8 @@ class DateTimePickerWidget extends StatefulWidget {
   final DateTime? maxDateTime;
   final DateTime? minDateTime;
   final bool startWithTime;
+  final bool showTitle;
+  final ValueChanged<bool>? onShowTimePickerChanged;
 
   const DateTimePickerWidget(
     this.onDateTimeSelected,
@@ -57,6 +45,8 @@ class DateTimePickerWidget extends StatefulWidget {
     this.maxDateTime,
     this.minDateTime,
     this.startWithTime = false,
+    this.showTitle = true,
+    this.onShowTimePickerChanged,
     super.key,
   });
 
@@ -78,35 +68,37 @@ class _DateTimePickerWidgetState extends State<DateTimePickerWidget> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = getEnteColorScheme(context);
+    final colors = context.componentColors;
     return Container(
-      color: colorScheme.backgroundElevated,
+      color: colors.backgroundBase,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           // Header
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                _showTimePicker
-                    ? AppLocalizations.of(context).selectTime
-                    : AppLocalizations.of(context).selectDate,
-                style: TextStyle(color: colorScheme.textBase, fontSize: 16),
+          if (widget.showTitle)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  _showTimePicker
+                      ? AppLocalizations.of(context).selectTime
+                      : AppLocalizations.of(context).selectDate,
+                  style: TextStyle(color: colorScheme.textBase, fontSize: 16),
+                ),
               ),
             ),
-          ),
 
           // Date/Time Picker
           Container(
             height: 220,
             decoration: BoxDecoration(
-              color: colorScheme.backgroundElevated2,
-              borderRadius: BorderRadius.circular(12),
+              color: colors.fillLight,
+              borderRadius: BorderRadius.circular(Radii.lg),
             ),
             child: CupertinoTheme(
               data: CupertinoThemeData(
-                brightness: Brightness.dark,
+                brightness: Theme.of(context).brightness,
                 textTheme: CupertinoTextThemeData(
                   dateTimePickerTextStyle: TextStyle(
                     color: colorScheme.textBase,
@@ -179,9 +171,7 @@ class _DateTimePickerWidgetState extends State<DateTimePickerWidget> {
                   onPressed: () {
                     if (_showTimePicker) {
                       // Go back to date picker
-                      setState(() {
-                        _showTimePicker = false;
-                      });
+                      _setShowTimePicker(false);
                     } else {
                       widget.onCancel();
                     }
@@ -207,9 +197,7 @@ class _DateTimePickerWidgetState extends State<DateTimePickerWidget> {
                       widget.onDateTimeSelected(_selectedDateTime);
                     } else {
                       // Move to time picker
-                      setState(() {
-                        _showTimePicker = true;
-                      });
+                      _setShowTimePicker(true);
                     }
                   },
                 ),
@@ -219,5 +207,12 @@ class _DateTimePickerWidgetState extends State<DateTimePickerWidget> {
         ],
       ),
     );
+  }
+
+  void _setShowTimePicker(bool showTimePicker) {
+    setState(() {
+      _showTimePicker = showTimePicker;
+    });
+    widget.onShowTimePickerChanged?.call(showTimePicker);
   }
 }
