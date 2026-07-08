838f0f0a19439cdecbed276aeaa430e34983820f
laurenspriem <laurens-pilot-git@lrns.me>
2026-07-08 12:57:08 +0530

    cargo test ml indexing mvp

---
+++ rust/crates/photos/tests/ml_indexing.rs
@@ -0,0 +1,20 @@
+mod support;
+
+use anyhow::Result;
+use support::ml_indexing::{MlIndexingTestContext, fail_if_any, run_with_large_stack};
+
+#[test]
+fn rust_ml_matches_python_goldens() {
+    run_with_large_stack("rust_ml_matches_python_goldens", run_ml_indexing_test);
+}
+
+fn run_ml_indexing_test() -> Result<()> {
+    let context = MlIndexingTestContext::load()?;
+    let runtime = context.prepare_runtime()?;
+
+    let mut failures = context.validate_manifest_expectations()?;
+    let rust_results = context.run_rust_indexing(&runtime, &mut failures)?;
+    context.compare_with_python_goldens(&rust_results, &mut failures)?;
+
+    fail_if_any(failures)
+}
