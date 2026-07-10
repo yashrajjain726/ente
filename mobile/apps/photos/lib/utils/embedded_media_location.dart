88b17c1d453850ae950a0d2fd61e294c79486b9f
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-07-10 23:24:50 +0530

    Share embedded location enrichment

---
+++ mobile/apps/photos/lib/utils/embedded_media_location.dart
@@ -0,0 +1,30 @@
+import "dart:io";
+
+import "package:exif_reader/exif_reader.dart";
+import "package:photos/models/file/extensions/file_props.dart";
+import "package:photos/models/file/file.dart";
+import "package:photos/models/location/location.dart";
+import "package:photos/utils/exif_util.dart";
+
+/// Adds Android location metadata to [file], preferring valid EXIF data.
+Future<void> updateLocationFromEmbeddedMetadata(
+  EnteFile file,
+  File sourceFile,
+  Map<String, IfdTag>? exifData,
+) async {
+  if (!Platform.isAndroid) {
+    return;
+  }
+  if (!file.hasLocation && file.isVideo) {
+    final videoLocation = (await getVideoPropsAsync(sourceFile))?.location;
+    if (videoLocation != null) {
+      file.location = videoLocation;
+    }
+  }
+  if (exifData != null) {
+    final exifLocation = locationFromExif(exifData);
+    if (Location.isValidLocation(exifLocation)) {
+      file.location = exifLocation;
+    }
+  }
+}
