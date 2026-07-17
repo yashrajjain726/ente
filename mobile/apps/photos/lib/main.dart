e848fbb7415349f074e7f42409cf3f71e52f9a55
laurenspriem <laurens-pilot-git@lrns.me>
2026-07-17 14:23:51 +0530

    [mobile][photos] Keep person cache warm-up non-fatal

--- mobile/apps/photos/lib/main.dart
+++ mobile/apps/photos/lib/main.dart
@@ -514,7 +514,12 @@ Future<void> _init(
     _logger.info("PushService/HomeWidget done $tlog");
     unawaited(MLService.instance.init());
     PersonService.init(entityService, MLDataDB.instance, preferences);
-    await PersonService.instance.refreshPersonCache();
+    try {
+      await PersonService.instance.refreshPersonCache();
+    } catch (e, s) {
+      PersonService.instance.clearCache();
+      _logger.severe("Person cache warm-up failed", e, s);
+    }
     if (!isBackground) {
       unawaited(_warmContactsCacheInBackground());
     }
