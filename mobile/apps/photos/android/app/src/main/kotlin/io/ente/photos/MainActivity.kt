db044cc9ac6a4db5dbff3fa14e98307ecfcb2471
Priyanshu Dangare <priyanshu@ente.com>
2026-07-30 12:52:57 +0530

    [mobile][photos] native channel dart binding

--- mobile/apps/photos/android/app/src/main/kotlin/io/ente/photos/MainActivity.kt
+++ mobile/apps/photos/android/app/src/main/kotlin/io/ente/photos/MainActivity.kt
@@ -2,8 +2,17 @@ package io.ente.photos
 
 import android.content.Intent
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        NativeChannel.register(
+            flutterEngine.dartExecutor.binaryMessenger,
+            contentResolver,
+        )
+    }
+
     override fun onNewIntent(intent: Intent) {
         setIntent(intent)
         super.onNewIntent(intent)
