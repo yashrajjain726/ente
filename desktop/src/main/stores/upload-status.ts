3b3e34a434cc4a518856072767ac8b463053cdf3
Manav Rathi <manav@mnvr.in>
2026-08-03 17:39:34 +0530

    Prune desktop comments

--- desktop/src/main/stores/upload-status.ts
+++ desktop/src/main/stores/upload-status.ts
@@ -2,32 +2,13 @@ import Store, { Schema } from "electron-store";
 import type { PreUploadSkippedFile, ZipItem } from "../../types/ipc";
 
 export interface UploadStatusStore {
-    /**
-     * The collection to which we're uploading, or the root collection.
-     *
-     * Not all pending uploads will have an associated collection.
-     */
     collectionName?: string;
-    /**
-     * Paths to regular files that are pending upload.
-     */
     filePaths?: string[];
-    /**
-     * Each item is the path to a zip file and the name of an entry within it.
-     */
     zipItems?: ZipItem[];
-    /**
-     * @deprecated Legacy paths to zip files, now subsumed into zipItems.
-     */
+    /** @deprecated Legacy paths to zip files, now subsumed into zipItems. */
     zipPaths?: string[];
-    /**
-     * Files that were skipped because either we could not open them (zip files)
-     * or they are hidden dot files.
-     */
     preUploadSkippedFiles?: PreUploadSkippedFile[];
-    /** Whether Takeout-favorited uploads should be added to Favorites. */
     importTakeoutFavorites?: boolean;
-    /** Whether media originating from Google Photos partner sharing is uploaded. */
     includePartnerSharedFiles?: boolean;
 }
 
--- desktop/src/main/stores/user-preferences.ts
+++ desktop/src/main/stores/user-preferences.ts
@@ -1,32 +1,11 @@
 import Store, { Schema } from "electron-store";
 
 interface UserPreferences {
-    /**
-     * If true, then the user has set a preference to also hide the dock icon on
-     * macOS when the window is closed using the x traffic light. The tray icon
-     * remains visible and can then be used to reopen the app when needed.
-     */
     hideDockIcon?: boolean;
     skipAppVersion?: string;
     muteUpdateNotificationVersion?: string;
-    /**
-     * The changelog version for which we last showed the "What's new" screen.
-     *
-     * See: [Note: Conditions for showing "What's new"]
-     */
     lastShownChangelogVersion?: number;
-    /**
-     * The last position and size of our app's window.
-     *
-     * This value is saved when the app is about to quit, and is used to restore
-     * the window to the previous state when it restarts. It is only saved if
-     * the app is not maximized (when the app was maximized when it was being
-     * quit then {@link isWindowMaximized} will be set instead).
-     */
     windowBounds?: { x: number; y: number; width: number; height: number };
-    /**
-     * `true` if the app's main window is maximized the last time it was closed.
-     */
     isWindowMaximized?: boolean;
 }
 
