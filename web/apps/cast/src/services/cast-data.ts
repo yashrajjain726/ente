6ba1dd53d3f6c0c99cfd47605e96887e3009fbb2
Manav Rathi <manav@mnvr.in>
2026-08-04 13:51:24 +0530

    Prune comments in the cast app

--- web/apps/cast/src/services/cast-data.ts
+++ web/apps/cast/src/services/cast-data.ts
@@ -1,11 +1,8 @@
 import type { CastPayload } from "./pair";
 
 export interface CastData {
-    /** The ID of the callection we are casting. */
     collectionID: string;
-    /** A key to decrypt the collection we are casting. */
     collectionKey: string;
-    /** A credential to use for fetching media files for this cast session. */
     castToken: string;
 }
 
@@ -14,18 +11,10 @@ export const clearCastData = () => {
         localStorage.removeItem(key);
 };
 
-/**
- * Save the data received after pairing with a sender into local storage.
- *
- * We will read in back when we start the slideshow.
- */
 export const storeCastData = (payload: CastPayload | undefined) => {
     if (!payload || typeof payload != "object")
         throw new Error("Unexpected cast data");
 
-    // Iterate through all the keys of the payload object and save them to
-    // localStorage. We don't validate here, we'll validate when we read these
-    // values back in `readCastData`.
     for (const [key, value] of Object.entries(payload)) {
         if (typeof value == "string" || typeof value == "number") {
             localStorage.setItem(key, value.toString());
@@ -35,12 +24,6 @@ export const storeCastData = (payload: CastPayload | undefined) => {
     }
 };
 
-/**
- * Read back the cast data we got after pairing.
- *
- * Sibling of {@link storeCastData}. It returns undefined if the expected data
- * is not present in localStorage.
- */
 export const readCastData = (): CastData | undefined => {
     const collectionID = localStorage.getItem("collectionID");
     const collectionKey = localStorage.getItem("collectionKey");
--- web/apps/cast/src/services/detect-type.ts
+++ web/apps/cast/src/services/detect-type.ts
@@ -2,17 +2,6 @@ import { lowercaseExtension } from "ente-base/file-name";
 import { KnownFileTypeInfos } from "ente-media/file-type";
 import { fileTypeFromBuffer } from "file-type";
 
-/**
- * Try to deduce the MIME type for the given {@link file}. Return the MIME type
- * string if successful _and_ if it is an image or a video, otherwise return
- * `undefined`.
- *
- * It first peeks into the file's initial contents to detect the MIME type. If
- * that doesn't give any results, it tries to deduce it from the file's name.
- *
- * For the list of returned extensions, see (for our installed version):
- * https://github.com/sindresorhus/file-type/blob/main/core.d.ts
- */
 export const detectMediaMIMEType = async (file: File) => {
     const chunkSizeForTypeDetection = 4100;
     const fileChunk = file.slice(0, chunkSizeForTypeDetection);
