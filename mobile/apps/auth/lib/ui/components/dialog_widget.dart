b7a30cb7a281c31ea2beef18b8e79a00b084a037
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-08-08 20:16:09 +0530

    Use shared button widget in Auth

--- mobile/apps/auth/lib/ui/components/dialog_widget.dart
+++ mobile/apps/auth/lib/ui/components/dialog_widget.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 
 import 'package:ente_auth/ui/components/buttons/button_component_adapter.dart';
-import 'package:ente_auth/ui/components/buttons/button_widget.dart';
-import 'package:ente_auth/ui/components/models/button_result.dart';
 import 'package:ente_base/typedefs.dart';
 import 'package:ente_components/ente_components.dart';
 import 'package:ente_strings/ente_strings.dart';
+import 'package:ente_ui/components/buttons/button_widget.dart';
+import 'package:ente_ui/components/buttons/models/button_result.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
