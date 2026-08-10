53dc1874e140aa4188c820e25cbe599251782c5d
Priyanshu Dangare <priyanshu@ente.com>
2026-08-10 18:22:38 +0530

    [mobile][photos] Clarify recoverable delete copy

--- mobile/apps/photos/lib/utils/delete_file_util.dart
+++ mobile/apps/photos/lib/utils/delete_file_util.dart
@@ -1147,6 +1147,8 @@ class _DeleteConfirmationSheet extends StatefulWidget {
   final Future<bool> Function() onDeleteFromRemote;
   final Future<bool> Function() onDeleteFromBoth;
 
+  const DeleteConfirmationSheet({super.key});
+
   const _DeleteConfirmationSheet({
     required this.isLocal,
     required this.isRemote,
