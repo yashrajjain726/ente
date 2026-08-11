437bf5c4afe00780ffbfd445976e50dd5dd4c3b0
Manav Rathi <manav@mnvr.in>
2026-08-11 15:16:59 +0530

    Prune server configuration comments

--- server/scripts/lint.sh
+++ server/scripts/lint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Run the configured linters.
-#
-# These checks will run when a PR is opened and new commits are pushed, but it
-# can also be run locally.
-
 set -o errexit
 set -o xtrace
 
