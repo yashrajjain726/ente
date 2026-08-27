00b1f7c430da8a86f45465b24a6ab43458cf92ec
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-08-27 11:53:22 +0530

    Add Photos and Locker file count state

---
+++ server/migrations/133_file_counts.down.sql
@@ -0,0 +1,9 @@
+SET lock_timeout = '5s';
+
+ALTER TABLE usage
+    DROP CONSTRAINT usage_file_counts_both_ready_or_both_unknown,
+    DROP COLUMN file_count_source_version,
+    DROP COLUMN locker_file_count,
+    DROP COLUMN photos_file_count;
+
+RESET lock_timeout;
---
+++ server/migrations/133_file_counts.up.sql
@@ -0,0 +1,11 @@
+SET lock_timeout = '5s';
+
+ALTER TABLE usage
+    ADD COLUMN photos_file_count BIGINT,
+    ADD COLUMN locker_file_count BIGINT,
+    ADD COLUMN file_count_source_version BIGINT NOT NULL DEFAULT 0,
+    ADD CONSTRAINT usage_file_counts_both_ready_or_both_unknown
+        CHECK ((photos_file_count IS NULL) = (locker_file_count IS NULL))
+        NOT VALID;
+
+RESET lock_timeout;
