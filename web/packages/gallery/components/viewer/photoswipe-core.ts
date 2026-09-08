2dbb87f34378bafe72a781da6059e31619612d70
Aswin Asok <aswinasokofficial@gmail.com>
2026-09-08 17:42:38 +0530

    remove-comments

--- web/packages/gallery/components/viewer/photoswipe-core.ts
+++ web/packages/gallery/components/viewer/photoswipe-core.ts
@@ -201,7 +201,6 @@ export class FileViewerPhotoSwipe<
                     const i = newIndex;
 
                     pswp.goTo(i);
-                    // Rebind the annotation to the refreshed destination slide.
                     _currentAnnotatedFile = undefined;
                     pswp.refreshSlideContent(i);
                     pswp.refreshSlideContent(i == 0 ? newFileCount - 1 : i - 1);
