0eb65ecf3b0576460f263c0ec143f52996f53e1e
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-09-18 13:00:01 +0530

    Keep offline opt-in unchanged

--- mobile/apps/auth/lib/core/configuration.dart
+++ mobile/apps/auth/lib/core/configuration.dart
@@ -94,15 +94,15 @@ class Configuration extends BaseConfiguration
   }
 
   Future<void> optForOfflineMode() async {
-    var offlineAuthKey = await _secureStorage.read(key: offlineAuthSecretKey);
-    if (offlineAuthKey == null) {
-      offlineAuthKey = CryptoUtil.bin2base64(CryptoUtil.generateKey());
+    if ((await _secureStorage.containsKey(key: offlineAuthSecretKey))) {
+      _offlineAuthKey = await _secureStorage.read(key: offlineAuthSecretKey);
+    } else {
+      _offlineAuthKey = CryptoUtil.bin2base64(CryptoUtil.generateKey());
       await _secureStorage.write(
         key: offlineAuthSecretKey,
-        value: offlineAuthKey,
+        value: _offlineAuthKey,
       );
     }
-    _offlineAuthKey = offlineAuthKey;
     await _preferences.setBool(hasOptedForOfflineModeKey, true);
   }
 
